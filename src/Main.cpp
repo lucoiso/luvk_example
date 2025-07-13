2c0a157fa12999bd55570fa3d3cb90b0554a95a2
lucoiso <lucas.oliveiravb@gmail.com>
2025-07-13 22:46:19 +0100

    adjustments

--- src/Main.cpp
+++ src/Main.cpp
@@ -59,7 +59,7 @@ int main()
                                            }),
                                            luvk::RendererEvents::OnRenderLoopInitialized);
 
-        Renderer->InitializeRenderLoop(DeviceModule, SwapChainModule, CommandPoolModule, MeshRegistryModule, ThreadPoolModule);
+        Renderer->InitializeRenderLoop();
 
         luvk::InitializeGlslang();
 
